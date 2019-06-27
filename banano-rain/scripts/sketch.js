7a0846ae73cd1fbcbde765e9e2a36ab4bdd31a98
milkyklim <klim.kolyvanov@gmail.com>
2019-06-27 18:50:29 +0200

    add final version; remove dead code; add adaptive logo;

--- banano-rain/scripts/sketch.js
+++ banano-rain/scripts/sketch.js
@@ -1,51 +1,39 @@
-// TODO: disable once done
-// p5.disableFriendlyErrors = true; // disables FES
+p5.disableFriendlyErrors = true;
 
-const sketch = ( s ) => {
+const sketch = (s) => {
   "use strict";
 
   const RainDrop = classes.RainDrop;
 
-  let raindropNum; 
   let rainDrops = [];
   let rainSplashes = [];
   let isPressed = false;
-
-  let hngShifts = [];
-  let hngN;
   
   let colorForeground = s.color('#FBDD11'); 
   let colorBackground = s.color('#2A2A2E');
-
-  // let bYellow = s.color('#FBDD11'); 
-  // let bGreen = s.color('#4CBF4B');
-  // let bGrey = s.color('#2A2A2E');
-  // let bDarkGrey = s.color('#212124');
   
-  let hng;
-  let banano;
-  let banano2;
-  let bt;
+  let hngImage;
+  let bananoLogo;
+  let bananoLogoInv;
+  let bananoLogoCurrent;
 
   s.preload = () => {
-    hng = s.loadImage('./assets/images/hng.svg');
-    banano = s.loadImage('./assets/images/banano-logo.svg');
-    banano2 = s.loadImage('./assets/images/banano-logo-2.svg');
+    hngImage = s.loadImage('./assets/images/hng.svg');
+    bananoLogo = s.loadImage('./assets/images/banano-logo.svg');
+    bananoLogoInv = s.loadImage('./assets/images/banano-logo-2.svg');
   }
 
   s.setup = () => {
     let divWidth = document.getElementById("rain").clientWidth;
     let divHeight = document.getElementById("rain").clientHeight;
-    let myCanvas = s.createCanvas(divWidth, divHeight);
-    myCanvas.parent('rain');
+    let sketchCanvas = s.createCanvas(divWidth, divHeight);
+    sketchCanvas.parent('rain');
 
     s.imageMode(s.CENTER);
-    bt = banano;
-
-    hngN = 7;
+    bananoLogoCurrent = bananoLogo;
 
     // s.createCanvas(800, 340);
-    raindropNum = Math.floor(s.windowWidth/3);
+    let raindropNum = Math.floor(s.windowWidth/2);
     for(let i = 0; i < raindropNum; i++)
       rainDrops.push(new RainDrop(s, colorBackground, colorForeground));
   }
@@ -53,14 +41,22 @@ const sketch = ( s ) => {
   s.draw = () => {
     // use the background color RGB values to create an RGBA value for the background
     s.background('rgba(' + s.red(colorBackground) + ',' 
-    + s.green(colorBackground) + ',' 
-    + s.blue(colorBackground) + ',' 
-    + 0.5 + ')');
+      + s.green(colorBackground) + ',' 
+      + s.blue(colorBackground) + ',' 
+      + 0.5 + ')');
 
-    s.image(hng, 0.8*s.width, 0.75*s.height);
+    s.image(hngImage, 0.8*s.width, 0.75*s.height);
 
-    // hngIt(10);
-    s.image(bt, s.width/2, s.height/4);
+    let scale = 1;
+    if (s.width < bananoLogoCurrent.width)
+      scale = s.width / bananoLogoCurrent.width;
+    if (s.height < bananoLogoCurrent.height){
+      tmp = s.height / bananoLogoCurrent.height;
+      scale = tmp < scale ? tmp : scale;
+    }
+
+    s.image(bananoLogoCurrent, s.width/2, s.height/4, 
+      scale*bananoLogoCurrent.width, scale*bananoLogoCurrent.height);
 
     for(let i = 0; i < rainDrops.length; i++) {
       let rs = rainDrops[i].update();
@@ -78,36 +74,17 @@ const sketch = ( s ) => {
   }
 
   s.mouseReleased = () => {
-    // s.image(hng, 500*s.random(), 500*s.random());
-    // console.log('released');
-
+    // random fast hng on screen
+    s.image(hngImage, s.width*s.random(), s.height*s.random());
     [colorBackground, colorForeground] = [colorForeground, colorBackground]
-    bt = isPressed ? banano : banano2;
+    bananoLogoCurrent = isPressed ? bananoLogo : bananoLogoInv;
     isPressed = !isPressed;
   }
 
-
   s.windowResized = () => {
-      var divWidth = document.getElementById("rain").clientWidth;
-      var divHeight = document.getElementById("rain").clientHeight;
+      let divWidth = document.getElementById("rain").clientWidth;
+      let divHeight = document.getElementById("rain").clientHeight;
       s.resizeCanvas(divWidth, divHeight);
-  }
-
-  const hngIt = (n=1) => {
-
-    let shift = s.width / (n);
-
-    for (let i = 1; i < n; i++){
-      s.image(hng, shift * (i), 
-        0.7*s.height);
-    }
-
-    // for (let i = 1; i < n; i++){
-    //   s.noiseSeed(i);
-    //   s.image(hng, shift * (i + s.noise(s.millis() / 1000)), 
-    //     0.7*s.height + shift*s.noise(s.millis() / 1000));
-    // }
-    
   }
 };
   
