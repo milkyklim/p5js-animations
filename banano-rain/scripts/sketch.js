12121a98aa07b456ba7a091a60d6ec90a72e55b4
milkyklim <klim.kolyvanov@gmail.com>
2019-06-27 11:33:23 +0200

    add resize sketch feature

--- banano-rain/scripts/sketch.js
+++ banano-rain/scripts/sketch.js
@@ -17,24 +17,22 @@ const sketch = ( s ) => {
     let isPressed = false; // bollean to check if mouse/screen was pressed
     
     // local but passed 
-    let colFG = s.color(0, 0, 0);
-    let colBG = s.color(255, 255, 255);
+    let colFG = s.color('#FBDD11'); // s.color(0, 0, 0);
+    let colBG = s.color('#2A2A2E'); // s.color(255, 255, 255);
 
-    let bYellow = s.color('#FBDD11');
+    let bYellow = s.color('#FBDD11'); 
     let bGreen = s.color('#4CBF4B');
     let bGrey = s.color('#2A2A2E');
     let bDarkGrey = s.color('#212124');
   
 
     s.setup = () => {      
-        // find the size of the underlying div
-        // var divWidth = $("#rain").width();
-        // var divHeight = $("#rain").height();
         // the canvas is defined as half the height and width of the window
-        // var myCanvas = createCanvas(divWidth, divHeight);
-      
-        s.createCanvas(800, 340);
-        // myCanvas.parent('rain');
+        let myCanvas = s.createCanvas(s.windowWidth, s.windowHeight);
+        myCanvas.parent('rain');
+
+        //  s.createCanvas(800, 340);
+
         // Populate the rainDrops array with RainDrop objects
         raindropNum = 800/3; //windowWidth/3;
         for(let i = 0; i < raindropNum; i++){
@@ -67,9 +65,6 @@ const sketch = ( s ) => {
           if(rainSplashes[j].change < rainSplashes[j].splashFrames){
             rainSplashes[j].update();
             rainSplashes[j].draw();
-
-
-            console.log(rainSplashes[j]);
             
           // else we update, draw it and then remove it from the array
           } else{
@@ -79,24 +74,19 @@ const sketch = ( s ) => {
           }
         }
       }
-      //w
+      
 
   
         //when the window is resized the canvas is resized accordingly
-        function windowResized(){
-            var divWidth = $('rain').width();
-            var divHeight = $('rain').height();
-            s.resizeCanvas(divWidth, divHeight);
+        s.windowResized = () => {
+            s.resizeCanvas(s.windowWidth, s.windowHeight);
         }
         // this function switches the foreground and background colors
-        function invertColor(){
-            var temp = colBG;
+        const invertColor = () => {
+            let temp = colBG;
             colBG = colFG;
             colFG = temp;
         }
-
-
-
   };
   
 let myp5 = new p5(sketch, 'rain');
