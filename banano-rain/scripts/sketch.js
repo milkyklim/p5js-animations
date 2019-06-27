b4c7bdefeab37b4e804b4ca17fff4ad84060b240
milkyklim <klim.kolyvanov@gmail.com>
2019-06-27 16:06:05 +0200

    add draft version with banano-logo and hng

--- banano-rain/scripts/sketch.js
+++ banano-rain/scripts/sketch.js
@@ -10,6 +10,9 @@ const sketch = ( s ) => {
   let rainDrops = [];
   let rainSplashes = [];
   let isPressed = false;
+
+  let hngShifts = [];
+  let hngN;
   
   let colorForeground = s.color('#FBDD11'); 
   let colorBackground = s.color('#2A2A2E');
@@ -19,11 +22,27 @@ const sketch = ( s ) => {
   // let bGrey = s.color('#2A2A2E');
   // let bDarkGrey = s.color('#212124');
   
+  let hng;
+  let banano;
+  let banano2;
+  let bt;
+
+  s.preload = () => {
+    hng = s.loadImage('./assets/images/hng.svg');
+    banano = s.loadImage('./assets/images/banano-logo.svg');
+    banano2 = s.loadImage('./assets/images/banano-logo-2.svg');
+  }
+
   s.setup = () => {
     let divWidth = document.getElementById("rain").clientWidth;
     let divHeight = document.getElementById("rain").clientHeight;
     let myCanvas = s.createCanvas(divWidth, divHeight);
     myCanvas.parent('rain');
+
+    s.imageMode(s.CENTER);
+    bt = banano;
+
+    hngN = 7;
 
     // s.createCanvas(800, 340);
     raindropNum = Math.floor(s.windowWidth/3);
@@ -32,21 +51,17 @@ const sketch = ( s ) => {
   }
 
   s.draw = () => {
-    //Check to see if screen or mouse are pressed
-    if(s.mouseIsPressed || s.touches.length != 0 /* || touchIsDown */) {
-      if(!isPressed) {
-        [colorBackground, colorForeground] = [colorForeground, colorBackground]
-        isPressed = true;
-      }
-    } 
-    else {
-      isPressed = false;
-    }
     // use the background color RGB values to create an RGBA value for the background
     s.background('rgba(' + s.red(colorBackground) + ',' 
-      + s.green(colorBackground) + ',' 
-      + s.blue(colorBackground) + ',' 
-      + 0.5 + ')');
+    + s.green(colorBackground) + ',' 
+    + s.blue(colorBackground) + ',' 
+    + 0.5 + ')');
+
+    s.image(hng, 0.8*s.width, 0.75*s.height);
+
+    // hngIt(10);
+    s.image(bt, s.width/2, s.height/4);
+
     for(let i = 0; i < rainDrops.length; i++) {
       let rs = rainDrops[i].update();
       if (rs != null)
@@ -61,11 +76,38 @@ const sketch = ( s ) => {
       } 
     }
   }
-  
+
+  s.mouseReleased = () => {
+    // s.image(hng, 500*s.random(), 500*s.random());
+    // console.log('released');
+
+    [colorBackground, colorForeground] = [colorForeground, colorBackground]
+    bt = isPressed ? banano : banano2;
+    isPressed = !isPressed;
+  }
+
+
   s.windowResized = () => {
       var divWidth = document.getElementById("rain").clientWidth;
       var divHeight = document.getElementById("rain").clientHeight;
       s.resizeCanvas(divWidth, divHeight);
+  }
+
+  const hngIt = (n=1) => {
+
+    let shift = s.width / (n);
+
+    for (let i = 1; i < n; i++){
+      s.image(hng, shift * (i), 
+        0.7*s.height);
+    }
+
+    // for (let i = 1; i < n; i++){
+    //   s.noiseSeed(i);
+    //   s.image(hng, shift * (i + s.noise(s.millis() / 1000)), 
+    //     0.7*s.height + shift*s.noise(s.millis() / 1000));
+    // }
+    
   }
 };
   
